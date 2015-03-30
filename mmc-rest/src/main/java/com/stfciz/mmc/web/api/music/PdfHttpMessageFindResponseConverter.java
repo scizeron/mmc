7bd2c12eedf21d85af2f5a15a39ab73e5539f299
scizeron <stephane.cizeron@gmail.com>
2015-03-30 17:00:01 +0200

    MAJ pdf

--- mmc-rest/src/main/java/com/stfciz/mmc/web/api/music/PdfHttpMessageFindResponseConverter.java
+++ mmc-rest/src/main/java/com/stfciz/mmc/web/api/music/PdfHttpMessageFindResponseConverter.java
@@ -21,6 +21,7 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
@@ -32,63 +33,85 @@ import com.itextpdf.text.pdf.PdfWriter;
  *
  * @param <T>
  */
-public class PdfHttpMessageFindResponseConverter extends AbstractHttpMessageConverter<FindResponse> {
+public class PdfHttpMessageFindResponseConverter extends
+    AbstractHttpMessageConverter<FindResponse> {
 
   private static final String NO_VALUE = "";
-  private ResourceBundle rating = ResourceBundle.getBundle("rating");
-  
+
+  private static final int TITLE_FONT_SIZE = 10;
+
+  private static final int HEADER_FONT_SIZE = 9;
+
+  private static final int ITEM_FONT_SIZE = 8;
+
+  private static final ResourceBundle RATING = ResourceBundle.getBundle("rating");
+
   /**
    * 
    */
   public PdfHttpMessageFindResponseConverter() {
-    super(MediaType.valueOf("application/pdf"));  
+    super(MediaType.valueOf("application/pdf"));
   }
-  
+
   @Override
   protected boolean supports(Class<?> clazz) {
     return true;
   }
 
   @Override
-  protected FindResponse readInternal(Class<? extends FindResponse> clazz, HttpInputMessage inputMessage) throws IOException, HttpMessageNotReadableException {
+  protected FindResponse readInternal(Class<? extends FindResponse> clazz,
+      HttpInputMessage inputMessage) throws IOException,
+      HttpMessageNotReadableException {
     return null;
   }
 
   @Override
-  protected void writeInternal(FindResponse findResponse, HttpOutputMessage outputMessage) throws IOException, HttpMessageNotWritableException {
+  protected void writeInternal(FindResponse findResponse,
+      HttpOutputMessage outputMessage) throws IOException,
+      HttpMessageNotWritableException {
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     OutputStream os = outputMessage.getBody();
     try {
+      Image yesImg = Image
+          .getInstance(this.getClass().getResource("/tick.png"));
+
       Document document = new Document();
       PdfWriter.getInstance(document, baos);
       document.open();
 
-      PdfPTable table = new PdfPTable(new float[] { 3, 5, 1, 2, 1, 1, 1, 1 });
+      PdfPTable table = new PdfPTable(new float[] { 3, 5, 1, 2, 1, 3, 2, 2 });
       table.setWidthPercentage(100f);
       table.getDefaultCell().setUseAscender(true);
       table.getDefaultCell().setUseDescender(true);
       // Add the first header row
-      Font f = new Font();
-      f.setSize(8);
-      f.setColor(BaseColor.WHITE);
-      PdfPCell cell = new PdfPCell(new Phrase(String.format("%s - %d item(s)"
-          , new SimpleDateFormat("yyyy/MM/dd").format(new Date())
-          , findResponse.getDocs().size()), f));
+      Font titleFont = new Font();
+
+      titleFont.setSize(TITLE_FONT_SIZE);
+      titleFont.setColor(BaseColor.WHITE);
+      PdfPCell cell = new PdfPCell(new Phrase(String.format("%s - %d item(s)",
+          new SimpleDateFormat("yyyy/MM/dd").format(new Date()), findResponse
+              .getDocs().size()), titleFont));
+
       cell.setBackgroundColor(BaseColor.BLACK);
       cell.setHorizontalAlignment(Element.ALIGN_CENTER);
       cell.setColspan(8);
       table.addCell(cell);
+
       // Add the second header row twice
+      Font headerFooterFont = new Font();
+      titleFont.setSize(HEADER_FONT_SIZE);
+      titleFont.setColor(BaseColor.BLACK);
+
       table.getDefaultCell().setBackgroundColor(BaseColor.LIGHT_GRAY);
       for (int i = 0; i < 2; i++) {
-          table.addCell("Artist");
-          table.addCell("Title");
-          table.addCell("Type");
-          table.addCell("Origin");
-          table.addCell("Year");
-          table.addCell("Re-edition");
-          table.addCell("Sleeve");
-          table.addCell("Record");
+        addHeaderCell(table, headerFooterFont, "Artist");
+        addHeaderCell(table, headerFooterFont, "Title");
+        addHeaderCell(table, headerFooterFont, "Type");
+        addHeaderCell(table, headerFooterFont, "Origin");
+        addHeaderCell(table, headerFooterFont, "Year");
+        addHeaderCell(table, headerFooterFont, "Re-edition");
+        addHeaderCell(table, headerFooterFont, "Sleeve");
+        addHeaderCell(table, headerFooterFont, "Record");
       }
       table.getDefaultCell().setBackgroundColor(null);
       // There are three special rows
@@ -96,50 +119,72 @@ public class PdfHttpMessageFindResponseConverter extends AbstractHttpMessageConv
       // One of them is a footer
       table.setFooterRows(1);
       // Now let's loop over the elements
-      
+
+      // new PdfPCell()
+
+      Font itemFont = new Font();
+      titleFont.setSize(ITEM_FONT_SIZE);
+      titleFont.setColor(BaseColor.BLACK);
+
       List<FindElementResponse> docs = findResponse.getDocs();
       for (FindElementResponse doc : docs) {
-        table.addCell(doc.getArtist());
-        table.addCell(doc.getTitle());
-        table.addCell(String.format("%s %s", (doc.getNbType() != null && doc.getNbType() > 1 ) ? String.valueOf(doc.getNbType()) : NO_VALUE, doc.getMainType()));
-        table.addCell(formatStringValue(doc.getOrigin()));
+        addHeaderCell(table, itemFont, doc.getArtist());
+        addHeaderCell(table, itemFont, doc.getTitle());
+        table.addCell(String.format("%s %s", (doc.getNbType() != null && doc
+            .getNbType() > 1) ? String.valueOf(doc.getNbType()) : NO_VALUE, doc
+            .getMainType()));
+        addHeaderCell(table, itemFont, doc.getOrigin());
         if (doc.getEdition() != null) {
           table.addCell(formatIntegerValue(doc.getEdition()));
-          table.addCell(String.format("%s", "y"));
+          table.addCell(new PdfPCell(yesImg, false));
         } else {
           table.addCell(formatIntegerValue(doc.getIssue()));
           table.addCell(NO_VALUE);
         }
-        table.addCell(formatRating(doc.getSleeveGrade()));
-        table.addCell(formatRating(doc.getRecordGrade()));
+        addHeaderCell(table, itemFont, formatRatingValue(doc.getSleeveGrade()));
+        addHeaderCell(table, itemFont, formatRatingValue(doc.getRecordGrade()));
       }
-      
+
       document.add(table);
       document.close();
-      
+
       StreamUtils.copy(baos.toByteArray(), os);
     } catch (DocumentException e) {
       throw new HttpMessageNotWritableException("Write internal error", e);
     }
   }
-  
-  private String formatRating(Integer value) {
-    if (value != null) {
-      return rating.getString(String.valueOf(value));
-    }
-    return NO_VALUE;
+
+  /**
+   * 
+   * @param table
+   * @param font
+   * @param value
+   */
+  private void addHeaderCell(PdfPTable table, Font font, Object value) {
+    String valueStr = value == null ? NO_VALUE : String.valueOf(value);
+    table.addCell(new PdfPCell(new Phrase(valueStr, font)));
   }
   
+  /**
+   * 
+   * @param value
+   * @return
+   */
+  private String formatRatingValue(Integer value) {
+   if (value != null) {
+     return RATING.getString(String.valueOf(value));
+   }
+   return NO_VALUE;
+  }
+
+  /**
+   * 
+   * @param value
+   * @return
+   */
   private String formatIntegerValue(Integer value) {
     if (value != null) {
       return String.format("%d", value);
-    }
-    return NO_VALUE;
-  }
-  
-  private String formatStringValue(String value) {
-    if (value != null) {
-      return value;
     }
     return NO_VALUE;
   }
