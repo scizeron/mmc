a138c762d29a22b848fbb989a3e64407d7d4f2c9
scizeron <stephane.cizeron@gmail.com>
2015-05-28 16:00:26 +0200

    AJOUT REQ_ID dans les logs

--- mmc-api/src/main/java/com/stfciz/mmc/web/controller/LoggingFilter.java
+++ mmc-api/src/main/java/com/stfciz/mmc/web/controller/LoggingFilter.java
@@ -1,12 +1,14 @@
 package com.stfciz.mmc.web.controller;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.MDC;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -17,13 +19,17 @@ public class LoggingFilter extends OncePerRequestFilter {
 
   private static Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
   
+  private static final String REQUEST_ID_MDC_ATTR = "REQ_ID";
+  
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
     try {
-      LOGGER.debug("{} >> {}", request.getRemoteAddr(), request.getRequestURL());
+      MDC.put(REQUEST_ID_MDC_ATTR, UUID.randomUUID().toString());
+      LOGGER.debug("remoteAddr: {}, secure: {}  >> {}", new Object[]{request.getRemoteAddr(), request.isSecure(), request.getRequestURI()});
       filterChain.doFilter(request, response);
     } finally {
-      LOGGER.debug("{} << {}", request.getRemoteAddr(), response.getStatus());
+      LOGGER.debug("remoteAddr: {}, secure: {} << {}", new Object[]{request.getRemoteAddr(), response.getStatus()});
+      MDC.remove(REQUEST_ID_MDC_ATTR);
     }
   }
 }
